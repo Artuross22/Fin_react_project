8df7bc3fb99b4c0e47a64de71a894bff361438a4
Artuross22 <bonjjjuaro@gmail.com>
2023-11-18 19:43:12 +0200

    added historical dividend

--- src/company.d.ts
+++ src/company.d.ts
@@ -43,6 +43,21 @@ export interface CompanySearch {
     isAdr: boolean;
     isFund: boolean;
   }
+
+  export interface CompanyHistoricalDividend {
+    symbol: string;
+    historical: Dividend[];
+  }
+  
+  export interface Dividend {
+    date: string;
+    label: string;
+    adjDividend: number;
+    dividend: number;
+    recordDate: string;
+    paymentDate: string;
+    declarationDate: string;
+  }
   export interface CompanyTenK {
     symbol: string;
     fillingDate: string;
